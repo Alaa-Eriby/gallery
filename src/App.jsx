44d2b1c54043d0d89b279d3f8abdc5c8c006fb51
Alaa-Eriby <alaa_elshehk@hotmail.com>
2024-08-31 14:52:08 +0300

    gallery

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Gallery from './component/Gallery/Gallery';
 import ImagePage from './component/ImagePage/ImagePage';
 import ImagesData from './component/Imagedata/Imagedata';
-import Navbar from './component/Navbar/Navbar'; // Import Navbar
-import './App.css'; // Import the CSS file
+import Navbar from './component/Navbar/Navbar'; 
+import './App.css'; 
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Add Navbar component here */}
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Gallery images={ImagesData} />} />
         <Route path="/image/:id" element={<ImagePage images={ImagesData} />} />
